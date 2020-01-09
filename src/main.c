5902be75bf686998a97034c10a9e28c77c8d3768
Vladislav Balachevskyi <vbalachevs@e2r9p2.unit.ua>
2020-01-09 21:18:58 +0200

    input and some structure

--- src/main.c
+++ src/main.c
@@ -35,33 +35,6 @@ static void escape_sequences(char* chars) {
     }
 }
 
-// static void setTerminalSettings() {
-//     t_termios newTerm;
-//     tcgetattr(STDIN_FILENO, &newTerm);
-//     newTerm.c_lflag &= ~(ICANON | ECHO);
-//     newTerm.c_lflag &= ~(ISIG);
-//     // newTerm.c_iflag &= ~INPCK;
-//     newTerm.c_iflag &= IGNCR; // IGNOR NEWLINE (return 13 on return)
-//     tcsetattr(STDIN_FILENO, 0, &newTerm);
-// }
-
-// static void disableTerminal(t_termios oldTerm) {
-//     tcsetattr(STDIN_FILENO, 0, &oldTerm);
-// }
-
-// unsigned int mx_getchar() {
-//     unsigned int ch = 0;
-//     t_termios oldTerm;
-
-//     tcgetattr(STDIN_FILENO, &oldTerm);
-//     setTerminalSettings();
-//     if (read(1, &ch, 3) == 0) {
-//         return 0;
-//     }
-//     disableTerminal(oldTerm);
-//     return ch;
-// }
-
 int mx_handleEvents(char ch) {
     if (ch == KEY_ENTER) {
         write(1, "\n", 1);
@@ -113,6 +86,13 @@ char* mx_input() {
     }
 }
 
+void mx_parsing(char *str) {
+
+}
+
+void mx_do_actions(char *str) {
+
+}
 
 int main(int argc, char *argv[], char *envp[]) {
     char *str = 0;
@@ -122,6 +102,8 @@ int main(int argc, char *argv[], char *envp[]) {
         if (str == 0)
             goto end_l;
         printf("str = %s\n", str);
+        mx_parsing(str);
+        mx_do_actions(str);
         free(str);
     }
     end_l:
