2d7765edd2d63248f40c75aec392dcf9d8743219
Serhii Olianovskyi <solianovsk@e2r10p5.unit.ua>
2020-02-17 10:41:59 +0200

    Audit Key_Tab

--- src/Key_TAB.c
+++ src/Key_TAB.c
@@ -7,48 +7,60 @@ static bool cmp_str_min_max(void *str1, void *str2) {
         return 0;
 }
 
-char **mx_key_tab(char *parsing, char **str, t_info *info) {
-    char *path = mx_mini_parser_tab(parsing, info);
-    t_list *list_comand = 0;
+static char **many_comand_in_list(char *path, char **str, t_info *info,
+                                  t_list *list_comand) {
+    int number_comand = 0;
     char **creat_list_comands = NULL;
-    int i = 0;
     int temp = 0;
+    int size_list = mx_list_size(list_comand) + 1;
+    char *name_comand = 0;
 
-    mx_read_comand(path, &list_comand);
-    list_comand = mx_sort_list(list_comand, &cmp_str_min_max);
-    if (!((temp = mx_list_size(list_comand)) == 0 || temp == 1)) {
-        mx_clean_monitor("", info, *str);
-        mx_print_Tab_comands(list_comand);
-        creat_list_comands = (char **)malloc((mx_list_size(list_comand) + 1) * sizeof(char *));
-        for (temp = mx_strlen(path) - 1; temp != 0 && path[temp] != '/'; temp--);
-        temp = mx_strlen(&path[temp]);
-        free(path);
-        free(parsing);
-        while (list_comand) {
-            path = list_comand->data;
-            creat_list_comands[i++] = mx_strdup(&path[temp - 1]);
-            mx_pop_front(&list_comand);
-        }
-        creat_list_comands[i++] = NULL;
+    mx_clean_monitor("", info, *str);
+    mx_print_Tab_comands(list_comand);
+    creat_list_comands = (char **)malloc(size_list * sizeof(char *));
+    for (temp = mx_strlen(path) - 1; temp != 0 && path[temp] != '/'; temp--);
+    temp = mx_strlen(&path[temp]);
+    while (list_comand) {
+        name_comand = list_comand->data;
+        creat_list_comands[number_comand++] = mx_strdup(&name_comand[temp - 1]);
+        mx_pop_front(&list_comand);
     }
-    else if (temp == 1) {
-        for (temp = mx_strlen(path) - 1; temp != 0 && path[temp] != '/'; temp--);
-        temp = mx_strlen(&path[temp]);
-        free(path);
-        free(parsing);
+    creat_list_comands[number_comand++] = NULL;
+    return creat_list_comands;
+}
+
+static void one_comand_in_list(t_info *info, t_list *list_comand, char **str,
+                               char *path) {
+    int len_comand = mx_strlen(path) - 1;
+
+    for (; len_comand != 0 && path[len_comand] != '/';)
+        len_comand--;
+        len_comand = mx_strlen(&path[len_comand]);
         path = list_comand->data;
-        for (i = temp - 1; path[i]; i++)
+        for (int i = len_comand - 1; path[i]; i++)
             mx_one_symbol(str, path[i], &(MX_STR_LEN), MX_STR_POS);
         if (MX_STR_POS != 0) {
             mx_one_symbol(str, ' ', &(MX_STR_LEN), MX_STR_POS);
             (MX_STR_POS)++;
         }
         mx_pop_front_free_data(&list_comand);
-    }
-    else {
+}
+
+char **mx_key_tab(char *parsing, char **str, t_info *info) {
+    char *path = mx_mini_parser_tab(parsing, info);
+    t_list *list_comand = 0;
+    char **creat_list_comand = NULL;
+    int comand_len = 0;
+
+    mx_read_comand(path, &list_comand);
+    list_comand = mx_sort_list(list_comand, &cmp_str_min_max);
+    if (!((comand_len = mx_list_size(list_comand)) == 0 || comand_len == 1))
+        creat_list_comand = many_comand_in_list(path, str, info, list_comand);
+    else if (comand_len == 1)
+        one_comand_in_list(info, list_comand, str, path);
+    else
         write(1,"\a", 1);
-        free(path);
-        free(parsing);
-    }
-    return creat_list_comands;
+    mx_strdel(&path);
+    mx_strdel(&parsing);
+    return creat_list_comand;
 }
--- src/mx_audit_str.c
+++ src/mx_audit_str.c
@@ -1,28 +1,5 @@
 #include "ush.h"
 
-static bool if_symbol(char test) {
-    if (test == 34 || test == 39 || test == 96)
-        return 1;
-    return 0;
-}
-
-// static void del_backslash_and_(char **str, t_info *processes) {
-//     char *new_str = 0;
-//     char temp[2];
-//     int i = 0;
-
-//     temp[1] = 0;
-//     while (str[0][i] != 0) {
-//         if (str[0][i] == 92)
-//             temp[0] = str[0][++i];
-//         new_str = mx_strjoin2(new_str, temp);
-//         i++;
-//     }
-//     mx_strdel(str);
-//     *str = new_str;
-// }
-
-static void do_replace(char **str, size_t start, size_t end, char *str_new);
 static void editor_str(char **str, t_info *processes) {
     char *temp = 0;
     char *temp2 = 0;
@@ -39,13 +16,8 @@ static void editor_str(char **str, t_info *processes) {
         mx_strdel(str);
         for (int i = 0; temp[i]; i++) 
             if (temp[i] == '\\' && (temp[i + 1] == '`' || temp[i + 1] == '\\'))
-                do_replace(&temp, i, i + 1, 0);
-        // temp2 = mx_audit_str(temp, processes, dqute);
-        // mx_strdel(&temp);
-        // mx_printerr(temp);
-        // mx_printerr("\n");
+                mx_do_replace(&temp, i, i + 1, 0);
         temp = mx_str_bquote(&temp, processes);
-            // temp2 = mx_audit_str(temp
         *str = temp;
     }
     else if (str[0][0] == 34) {
@@ -56,38 +28,12 @@ static void editor_str(char **str, t_info *processes) {
     }
 }
 
-static void do_replace(char **str, size_t start, size_t end, char *str_new) {
-    char *newStr = NULL;;
-    size_t i = mx_strlen(*str);
-    int j = end;
-    int sum = 0;
-
-    if (str_new)
-        sum = mx_strlen(str_new);
-    // if (!(sum ==  0 && i == end - start))
-    newStr = (char*) malloc (i - (end - start) + sum + 1);
-    if (newStr) {
-        for (i = 0; i < start; i++) {
-            newStr[i] = (*str)[i];
-        }
-        while(sum != 0 && str_new[i - start]) {
-            newStr[i] = str_new[i - start];
-            i++;
-        }
-        while((*str)[j])
-            newStr[i++] = (*str)[j++];
-        newStr[i] = 0;
-    }
-    free(*str);
-    *str = newStr;
-    // mx_printstr(newStr);
-    // mx_printstr("||\n");
-}
-
 static bool is_not_operator(char c) {
     if (c == '|' || c == '&' || c == '>' || c == '<'|| c == '$' || c == ' ')
         return false;
-    if (c == '='|| c == 92 || c == 34 || c == 39 || c== 96 || c == 0 || c== '?')
+    if (c == 34 || c == 39 || c== 96 || c == 0 || c== '?')
+        return false;
+    if (c == '='|| c == 92)
         return false;
     return true;
 }
@@ -97,9 +43,27 @@ static int end_parametr(char *str, int i) {
 
     while (is_not_operator(str[temp]))
         temp++;
-    if (str[temp] == '?')
+    if (str[temp] == '?' && str[temp + 1] != '(')
         temp++;
     return temp;
+}
+
+// static bool if_symbol(char test) {
+//     if (test == 34 || test == 39 || test == 96)
+//         return 1;
+//     return 0;
+// }
+static bool chek_comand(char *new_str, int i) {
+    if (new_str[i] == 34 || new_str[i] == 39 || new_str[i] == 96) {
+        if (i == 0 || new_str[i - 1] != '\'' || !mx_check_symbol(new_str, i, new_str[i]))
+            return true;
+        else
+            return false;
+    }
+    else if (new_str[i + 1] == '(' && (i == 0 || new_str[i - 1] != '\''  || !mx_check_symbol(new_str, i, '$')))
+        return true;
+    else
+        return false;
 }
 
 char *mx_audit_str(char *str, t_info *processes, bool dqute) {
@@ -107,18 +71,16 @@ char *mx_audit_str(char *str, t_info *processes, bool dqute) {
         return 0;
     char *new_str = mx_strdup(str);
     char *temp = NULL;
-    // int pos = 0;
     int flag = 0;
-    // mx_printerr(str);
-    //         mx_printerr("\n");
-    // // mx_printstr();
-    for (int i = 0, pos = 0; new_str && new_str[i]; i++, pos = i) {
-            // mx_printerr(new_str);
+    int i = 0;
+    int pos = 0;
+
+    for (; new_str && new_str[i]; i++, pos = i) {
         if (new_str[i] == '~' && !dqute)
             mx_HOME(&new_str, &i, processes);
-            // mx_printerr("\n");
-        else if ((if_symbol(new_str[i]) && (i == 0 || new_str[i - 1] != '\'' || !mx_check_symbol(new_str, i, new_str[i])))
-            || (new_str[i + 1] == '(' && (i == 0 || new_str[i - 1] != '\''  || !mx_check_symbol(new_str, i, '$')))) {
+        // else if ((if_symbol(new_str[i]) && (i == 0 || new_str[i - 1] != '\'' || !mx_check_symbol(new_str, i, new_str[i])))
+        //     || (new_str[i + 1] == '(' && (i == 0 || new_str[i - 1] != '\''  || !mx_check_symbol(new_str, i, '$')))) {
+        else if (chek_comand(new_str, i)) {
             pos++;
             if (new_str[i] == '$') {
                 pos++;
@@ -129,7 +91,7 @@ char *mx_audit_str(char *str, t_info *processes, bool dqute) {
             mx_end_flag(new_str, &pos, mx_strlen(new_str), flag);
             temp = mx_strndup(&new_str[i], pos - i);
             editor_str(&temp, processes);
-            do_replace(&new_str, i, pos, temp);
+            mx_do_replace(&new_str, i, pos, temp);
             if (temp) {
                 i = i + mx_strlen(temp);
                 mx_strdel(&temp);
@@ -145,18 +107,13 @@ char *mx_audit_str(char *str, t_info *processes, bool dqute) {
             }
             else
                 temp = mx_return_value(&temp, &(processes->var_tree));
-            do_replace(&new_str, i, flag, temp);
+            mx_do_replace(&new_str, i, flag, temp);
             if (temp)
                 i = i + mx_strlen(temp);
             i--;
         }
-        else if (new_str[i] == '\\' && (!dqute || (dqute && (new_str[i + 1] == '\\')))) {
-            //  mx_printerr(new_str);
-            // mx_printerr("\n");
-            do_replace(&new_str, i, i + 1, 0);
-        }
-            //                 mx_printerr(new_str);
-            // mx_printerr("\n");
+        else if (new_str[i] == '\\' && (!dqute || (dqute && (new_str[i + 1] == '\\'))))
+            mx_do_replace(&new_str, i, i + 1, 0);
     }
     return new_str;
 }
