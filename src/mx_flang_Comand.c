eb37ed4ddb6513d56888ce279ee2261a1ffadffe
Serhii Olianovskyi <solianovsk@e2r10p5.unit.ua>
2020-02-17 19:54:41 +0200

    fix

--- src/mx_flang_Comand.c
+++ src/mx_flang_Comand.c
@@ -15,10 +15,10 @@ static void printerrflag(char flag) {
 static bool flag_oper(char *str, int *pos, int flag) {
     if (flag != ' ')
         return 0;
-    if (str[*pos] == '|' || str[*pos] == '<'
-        || str[*pos] == '>' || str[*pos] == '&')
-        if (mx_check_symbol(str, *pos, str[*pos])) {
-            (*pos)--;
+    if (str[pos[0]] == '|' || str[pos[0]] == '<'
+        || str[pos[0]] == '>' || str[pos[0]] == '&')
+        if (mx_check_symbol(str, pos[0], str[pos[0]])) {
+            pos[0]--;
             return 1;
         }
     return 0;
@@ -27,13 +27,13 @@ static bool flag_oper(char *str, int *pos, int flag) {
 static int new_spec_symbol(char *str, int *pos, int end, int flag) {
     int new_flag = flag;
 
-    if (if_symbol(str[*pos]) && mx_check_symbol(str, *pos, str[*pos])) {
-        (*pos)++;
+    if (if_symbol(str[pos[0]]) && mx_check_symbol(str, pos[0], str[pos[0]])) {
+        pos[0]++;
         new_flag = mx_end_flag(str, pos, end, new_flag);
     }
-    else if (*pos > 0 && str[*pos] == 40 
-             && mx_check_symbol(str, *pos - 1, '$')) {
-        (*pos)++;
+    else if (pos[0] > 0 && str[*pos] == 40 
+             && mx_check_symbol(str, pos[0] - 1, '$')) {
+        pos[0]++;
         new_flag = mx_end_flag(str, pos, end, ')');
     }
     return new_flag;
@@ -42,20 +42,18 @@ static int new_spec_symbol(char *str, int *pos, int end, int flag) {
 int mx_flang_Comand(char *str, int *pos, int end, int flag) {
     int check_flag = flag;
 
-    while (*pos <= end) {
-        check_flag = str[*pos];
-        if (mx_check_symbol(str, *pos, flag) || flag_oper(str, pos, flag)) {
-            if (check_flag == ')')
-                (*pos)++;
+    while (pos[0] <= end) {
+        check_flag = str[pos[0]];
+        if (mx_check_symbol(str, pos[0], flag) || flag_oper(str, pos, flag)) {
             return 0;
         }
         else if ((if_symbol(str[*pos])
-                  && mx_check_symbol(str, *pos, str[*pos]))
-                  || (*pos > 0 && str[*pos] == 40 
-             && mx_check_symbol(str, *pos - 1, '$')))
+                  && mx_check_symbol(str, pos[0], str[pos[0]]))
+                  || (pos[0] > 0 && str[pos[0]] == 40 
+             && mx_check_symbol(str, pos[0] - 1, '$')))
              check_flag = new_spec_symbol(str, pos, end, check_flag);
         else
-            (*pos)++;
+            (pos[0])++;
     }
     if (flag != 32)
         printerrflag(flag);
