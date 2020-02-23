d175eff50b0d02e6275f66e11043298adf8bee95
Serhii Olianovskyi <solianovsk@e2r10p5.unit.ua>
2020-02-23 20:40:44 +0200

    fix

--- libmx/src/mx_pop_list.c
+++ libmx/src/mx_pop_list.c
@@ -1,6 +1,6 @@
 #include "libmx.h"
 
-static bool del(t_list *s, t_list **head) {
+static bool del(t_list *s, t_list **head, void(*del_data)(void *)) {
     del_data(s->data);
     mx_pop_front_free_data(head);
     return 1;
@@ -14,7 +14,7 @@ void mx_pop_list(t_list **head, void *data, bool(*if_list)(void *, void *),
     if (head == 0 || *head == 0)
         return;
     s = *head;
-    if (if_list(data, s->data) && del(s, head))
+    if (if_list(data, s->data) && del(s, head, del_data))
         return;
     while (s->next != 0) {
         if (if_list(data, s->next->data)) {
