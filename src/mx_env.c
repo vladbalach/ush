b54495bfc51e289ac7b5c0d7b9f14181418bd078
Vladyslav Struk <vstruk@e2r9p4.unit.ua>
2020-02-23 16:57:45 +0200

    fix env

--- src/mx_env.c
+++ src/mx_env.c
@@ -1,13 +1,10 @@
 #include "ush.h"
 
-static void push_back(t_var *var, char *str)
-{
+static void push_back(t_var *var, char *str) {
     char *name = strndup(str, mx_get_char_index(str, '='));
 
-    while (var->next)
-    {
-        if (strcmp(var->name, name) == 0)
-        {
+    while (var->next) {
+        if (strcmp(var->name, name) == 0) {
             free(var->value);
             var->value = strdup(str);
             free(name);
@@ -24,47 +21,39 @@ static void push_back(t_var *var, char *str)
     var->next = NULL;
 }
 
-static bool print_env(t_var *var, char **argv, int *i)
-{
-    if (argv[*i] == NULL)
-    {
+static bool print_env(t_var *var, char **argv, int *i) {
+    if (argv[*i] == NULL) {
         mx_print_env(var);
         return 1;
     }
-    for (; argv[*i]; (*i)++)
-    {
+    for (; argv[*i]; (*i)++) {
         if (mx_reg(argv[*i], MX_REG_VER))
             push_back(var, argv[*i]);
         else
             break;
     }
-    if (argv[*i] == NULL)
-    {
+    if (argv[*i] == NULL) {
         mx_print_env(var);
         return 1;
     }
     return 0;
 }
 
-void mx_env(char **argv, t_info *info)
-{
+void mx_env(char **argv, t_info *info) {
     t_var *var = mx_var_tree_to_var(info->var_tree);
     char *path = NULL;
     int i = 0;
 
-    for (i = 1; argv[i] != NULL; i++)
-    {
+    for (i = 1; argv[i] != NULL; i++) {
         if (mx_reg(argv[i], MX_REG_VER) || mx_reg(argv[i], MX_REG_PROG))
             break;
-        if (mx_check_env(argv, &path, var, &i))
-        {
+        if (mx_check_env(argv, &path, var, &i)) {
             mx_fre_env_path(var, path);
             info->lastStatus = 1;
             return;
         }
     }
-    if (print_env(var, argv, &i))
-    {
+    if (print_env(var, argv, &i)) {
         mx_fre_env_path(var, path);
         return;
     }
